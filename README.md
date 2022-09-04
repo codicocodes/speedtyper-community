0dee4faa85daf1fc946c7343b556a853a04e42a7
codicocodes <76068197+codicocodes@users.noreply.github.com>
2022-09-04 10:03:32 +0000

    Deploying to gh-pages from @ codicocodes/speedtyper-community@1f6224b2b46a66fb74b4496b1c376db3a7891d1a 🚀

--- README.md
+++
@@ -1,1 +0,0 @@
-# **❗ This repository is deprecated, please see [this](https://github.com/codicocodes/speedtyper.dev) repository to contribute to speedtyper.**
--- buildFile.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-const challenges = require("./challenges");
-
-fs.writeFile("build/index.json", JSON.stringify(challenges), "utf8", () => {
-  console.log("successful :)");
-});
